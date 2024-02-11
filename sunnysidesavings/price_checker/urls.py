bfee82e4b9c1c4d29deb87689e43ae77675eb8ff
aexko <77161930+aexko@users.noreply.github.com>
2024-02-11 09:45:16 -0500

    filter works

--- sunnysidesavings/price_checker/urls.py
+++ sunnysidesavings/price_checker/urls.py
@@ -1,12 +1,11 @@
 # import some urls
 from django.urls import path
-
 from . import views
 
-
-# create a list of urls
 urlpatterns = [
     path("", views.price_checker_render, name="price_checker"),
-    path("get_grocery_items", views.get_grocery_items, name="get_grocery_items"),
-    path("get_grocery_items_by_name", views.get_grocery_items_by_name, name="get_grocery_items_by_name"),
-    path("get_grocery_items_by_grocery_stores", views.get_grocery_items_by_grocery_stores, name="get_grocery_items_by_grocery_stores"),]
+    # class views for the grocery items
+    path("search_grocery_items", views.GroceryItemView.as_view(), name="search_grocery_items"),
+    # path("get_grocery_items", views.get_grocery_items, name="get_grocery_items"),
+    path("get_grocery_items_by_name", views.GroceryItemView.as_view(), name="get_grocery_items_by_name"),
+]
--- sunnysidesavings/price_checker/views.py
+++ sunnysidesavings/price_checker/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import GroceryItem, GroceryStore
+from django.views.generic import ListView
 
 
 # rendering a list of grocery items
 def price_checker_render(request):
     return render(request, "index.html")
 
+
 # return all grocery items
 def get_grocery_items(request):
     grocery_items = GroceryItem.objects.all()
     print(grocery_items)
     return HttpResponse(grocery_items)
+
 
 # filtering grocery items by name that user inputs
 def get_grocery_items_by_name(request):
@@ -21,9 +24,14 @@ def get_grocery_items_by_name(request):
     return HttpResponse(grocery_items)
 
 
-# rendering a list of grocery items by grocery stores
-def get_grocery_items_by_grocery_stores(request):
-    grocery_items = GroceryItem.objects.filter(grocery_store__name__icontains=request.GET["name"])
-    print(grocery_items)
-    return HttpResponse(grocery_items)
+# class view for the grocery items, it shows all the grocery items filtered by the name
+class GroceryItemView(ListView):
+    model = GroceryItem
+    template_name = "grocery_items.html"
+    context_object_name = "grocery_items"
 
+    def get_queryset(self):
+        query = self.request.GET.get("search")
+        print("query: ", query)
+        print("things returned: ", GroceryItem.objects.filter(name__icontains=query).order_by("price_per_unit"))
+        return GroceryItem.objects.filter(name__icontains=query).order_by("price_per_unit")
