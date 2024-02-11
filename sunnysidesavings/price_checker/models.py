f634f56d74528b16dc4de6e598cd80a6839b3642
aexko <77161930+aexko@users.noreply.github.com>
2024-02-11 01:28:19 -0500

    separation of grocery_store and grocery_item + imagefield

--- sunnysidesavings/price_checker/models.py
+++ sunnysidesavings/price_checker/models.py
@@ -5,26 +5,48 @@ from django.db import models
 class GroceryItem(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    grocery_store = models.CharField(max_length=100)
     date_start = models.DateField()
     date_end = models.DateField()
     quantity = models.IntegerField()
     price_per_unit = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to="images/", blank=True)
 
-    # Create a string representation of the grocery item
     def __str__(self):
         return (
-            self.name
-            + " "
-            + str(self.price)
-            + " "
-            + self.grocery_store
-            + " "
-            + str(self.date_start)
-            + " "
-            + str(self.date_end)
-            + " "
-            + str(self.quantity)
-            + " "
-            + str(self.price_per_unit)
+                self.name
+                + " "
+                + str(self.price)
+                + " "
+                + self.grocery_store
+                + " "
+                + str(self.date_start)
+                + " "
+                + str(self.date_end)
+                + " "
+                + str(self.quantity)
+                + " "
+                + str(self.price_per_unit)
+        )
+
+
+# Create a class for the grocery store
+class GroceryStore(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to="images/", blank=True)
+
+    def __str__(self):
+        return (
+                self.name
+                + " "
+                + self.address
+                + " "
+                + self.city
+                + " "
+                + self.state
+                + " "
+                + self.zip_code
         )
