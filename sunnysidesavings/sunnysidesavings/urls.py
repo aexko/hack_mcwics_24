d91b58735d379554d1b662b71989920b2cf38001
aexko <77161930+aexko@users.noreply.github.com>
2024-02-10 23:53:05 -0500

    adding a name of url for the navbar

--- sunnysidesavings/sunnysidesavings/urls.py
+++ sunnysidesavings/sunnysidesavings/urls.py
@@ -21,6 +21,6 @@ from bootstrap_colors.views import BootstrapColorsView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("price-checker/", include("price_checker.urls")),
+    path("price-checker/", include("price_checker.urls"), name="home"),
     path("colors.css", BootstrapColorsView.as_view(), name="colors"),
 ]
