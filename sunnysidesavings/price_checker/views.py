2d7bba9a56e1b46e4940c99e6fcef648fb430988
aexko <77161930+aexko@users.noreply.github.com>
2024-02-10 20:03:56 -0500

    TEMPLATE WORKS

--- sunnysidesavings/price_checker/views.py
+++ sunnysidesavings/price_checker/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# import requests
 
 # Create your views here.
-def testing(request):
-    return HttpResponse("Hello, world. You're at the price_checker index.")
+# def testing(request):
+#     return HttpResponse("Hello, world. You're at the price_checker index.")
+
+# return the index page
+def priceCheckerPage(request):
+    return render(request, "index.html")
--- sunnysidesavings/sunnysidesavings/settings.py
+++ sunnysidesavings/sunnysidesavings/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,10 +54,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "sunnysidesavings.urls"
 
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Montreal"
 
 USE_I18N = True
 
@@ -117,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
