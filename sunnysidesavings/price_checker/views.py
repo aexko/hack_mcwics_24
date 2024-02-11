d256e2e1992465e87f05767c489f1a6b858566f4
aexko <77161930+aexko@users.noreply.github.com>
2024-02-11 02:27:19 -0500

    [problem] we need to get the logic good for filtering

--- sunnysidesavings/price_checker/views.py
+++ sunnysidesavings/price_checker/views.py
@@ -13,11 +13,14 @@ def get_grocery_items(request):
     print(grocery_items)
     return HttpResponse(grocery_items)
 
-# rendering a list of grocery items by name
+# filtering grocery items by name that user inputs
 def get_grocery_items_by_name(request):
-    grocery_items = GroceryItem.objects.filter(name__icontains=request.GET["name"])
+    grocery_items = GroceryItem.objects.all()
     print(grocery_items)
-    return HttpResponse(grocery_items)
+    print("request", request)
+    print("request.GET", request.GET)
+    return ""
+
 
 # rendering a list of grocery items by grocery stores
 def get_grocery_items_by_grocery_stores(request):
